edba3da626e5cd1eea43b06b3bd95edf9dd64316
moudy99 <bobmoudy1@gmail.com>
2024-05-01 19:38:17 +0300

    add preloadImages

--- js/main.js
+++ js/main.js
@@ -82,3 +82,174 @@
     },
   });
 })(jQuery);
+
+// ? Cashing all Images
+
+var Teleferic = [
+  "img/Sectores/Teleferic/Al-noor.jpg",
+  "img/Sectores/Teleferic/pic-tunektepe-teleferik-antalya-4.jpeg",
+  "img/Sectores/Teleferic/Al-Thore.jpg",
+  "img/Sectores/Teleferic/Teleferik,_Uludağ.jpg",
+  "img/Sectores/Teleferic/Al-noor-2.jpg",
+  "img/Sectores/Teleferic/Al-Thore-2.jpg",
+];
+
+var AquaPark = [
+  // Aqua park
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-2jpg.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-video-1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-video-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional%20.jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional-1.jpeg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Sub%20Catamaran--1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Sub%20Catamaran--2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/sub-Catamaran-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-1.jpeg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-5.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-2jpg.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-5.jpg",
+];
+var Freeis_wheels = [
+  "img/Sectores/Freeis-wheels/Ferris-wheels-5.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-1.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-7.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-2.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-3.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-4.jpg",
+  "img/Sectores/Freeis-wheels/Ferris-wheels-6.jpg",
+];
+var skiDome = [
+  //skiDome
+
+  "img/Sectores/skiDome/ski-domes-in-Malaysia.jpg",
+  "img/Sectores/skiDome/Feasibility Study .png",
+  "img/Sectores/skiDome/ice-hotel-1.jpeg",
+  "img/Sectores/skiDome/ice-hotel.jpg",
+  "img/Sectores/skiDome/ice-hotel-2I.webp",
+  "img/Sectores/skiDome/ice-hotel-2.webp",
+  "img/Sectores/skiDome/indoor-ski-1.jpg",
+  "img/Sectores/skiDome/indoor-ski-2.jpg",
+  "img/Sectores/skiDome/indoor-ski-3.jpg",
+  "img/Sectores/skiDome/indoor-ski-4.webp",
+  "img/Sectores/skiDome/Ice-Cave-2.jpg",
+  "img/Sectores/skiDome/Ice-Cave-4.jpg",
+  "img/Sectores/skiDome/ice-cave-1.jpg",
+];
+var Playground = [
+  //Playground
+
+  "img/Sectores/Playground/playground-1.jpg",
+  "img/Sectores/Playground/playground-2.jpg",
+  "img/Sectores/Playground/playground-3.jpg",
+  "img/Sectores/Playground/playground-4.jpg",
+  "img/Sectores/Playground/playground-5.jpg",
+  "img/Sectores/Playground/playground-6.jpg",
+  "img/Sectores/Playground/playground-video-1.jpg",
+  "img/Sectores/Playground/playground-video-2.jpg",
+  "img/Sectores/Playground/playground-video-3.jpg",
+  "img/Sectores/Playground/trampoline-6.webp",
+  "img/Sectores/Playground/Trampoline-1.jpg",
+  "img/Sectores/Playground/trampoline-5.jpeg",
+  "img/Sectores/Playground/trampoline-3.jpg",
+  "img/Sectores/Playground/trampoline-4.jpg",
+  "img/Sectores/Playground/Theme-Park.-.jpg",
+  "img/Sectores/Playground/Theme-Park.-1.jpg",
+  "img/Sectores/Playground/Theme-Park.-2.jpg",
+  "img/Sectores/Playground/Theme-Park.-3.jpg",
+  "img/Sectores/Playground/Theme-Park.-4.jpg",
+  "img/Sectores/Playground/Theme-Park.-5.jpg",
+  "img/Sectores/Playground/edutainment-1.jpg",
+  "img/Sectores/Playground/edutainment-2.jpg",
+  "img/Sectores/Playground/edutainment-3.webp",
+];
+var safari_zoo = [
+  //Safari Zoo
+  "img/Sectores/safari-zoo/icons/wild-animals.png",
+  "img/Sectores/safari-zoo/icons/horse.png",
+  "img/Sectores/safari-zoo/icons/dolphin.png",
+  "img/Sectores/safari-zoo/icons/parrot.png",
+  "img/Sectores/safari-zoo/icons/cowboy-hat.png",
+  "img/Sectores/safari-zoo/icons/elephant.png",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-Zoo1.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/Safari-zoo3.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/Safari-zoo2.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-1.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-2.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-3.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-5.jpeg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-6.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-7.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-9.jpg",
+  "img/Sectores/safari-zoo/sectors-sliders/safari-zoo-video-cover-4.jpg",
+];
+var SubMarines = [
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-2jpg.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-video-1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Luxury-Yacht-video-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional .jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/conventional-1.jpeg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Semi-Submarine-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Sub Catamaran--1.jpg",
+  "img/Sectores/Submarines/sectors-sliders/Sub Catamaran--2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/sub-Catamaran-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-1.jpeg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-2.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/parasailing-5.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-2jpg.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-3.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-4.jpg",
+  "img/Sectores/Submarines/sectors-sliders/glass-boat-5.jpg",
+];
+
+var imageUrlsByPage = {
+  // "/index.html": Teleferic,
+  "/Indoor_ski_dome.html": skiDome,
+  "/Submarines.html": SubMarines,
+  "/Playground.html": Playground,
+  "/Teleferik.html": Teleferic,
+  "/safari_zoo.html": safari_zoo,
+  "/Aqua_park.html": AquaPark,
+  "/Ferris_wheels.html": Freeis_wheels,
+};
+
+function preloadImagesForCurrentPage() {
+  var currentPage = window.location.pathname;
+  var currentPageImages = imageUrlsByPage[currentPage];
+  if (currentPageImages) {
+    currentPageImages.forEach(function (url) {
+      var img = new Image();
+      img.src = url;
+      img.onload = function () {
+        var cachedImg = new Image();
+        cachedImg.src = url;
+        if (!cachedImg.complete) {
+          console.log("Image loaded from server:", url);
+        }
+      };
+      img.onerror = function () {
+        console.error("Failed to load image:", url);
+      };
+
+      // Add caching headers to force caching
+      img.crossOrigin = "anonymous";
+      img.setAttribute("referrerpolicy", "no-referrer");
+      img.setAttribute("loading", "lazy");
+    });
+  }
+}
+
+window.onload = preloadImagesForCurrentPage;
